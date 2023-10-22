2732cebcc10c46a71efc3b61d79ed09773d8e287
Eryk Mika <erykmika108@gmail.com>
2023-10-22 16:06:33 +0200

    Opracowanie zalozen projektowych

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,10 @@
+Dokumentacja projektu miniUSOS
+==================================================
+
+
+.. toctree::
+    zalozenia
+    :maxdepth: 2
+    :caption: Zawartość:
+
+:Course: Databases II
---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Projekt miniUSOS'
+copyright = '2023, Mika, Łabowicz, Hajnowski'
+author = 'Mika, Łabowicz, Hajnowski'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pl'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+html_style = 'styles/custom.css'
